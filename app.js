dee003154ec8e4bb667e0fcd15627bbeda765eee
Ananthakrishnan U S <67046321+anan121314@users.noreply.github.com>
2022-08-10 22:38:35 +0530

    config file

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ app.get('/',function(req,res){
 
 
 //#part1 point5
-app.listen(5000,()=>{
-    console.log("Server Ready on 5000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
